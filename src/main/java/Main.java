673bd82262be4380ecc5bec202c066489f79b15e
DayvsonNascimento <dayvson.nascimento@ccc.ufcg.edu.br>
2018-11-09 01:35:01 -0300

    Remoção de código desnecessário

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         GraphBuilder graphBuilder = new GraphBuilder();
-        System.out.println();
+        graphBuilder.build();
     }
 
 }
